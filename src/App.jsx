84be640ec13294653cbcf422d7192ade4403b021
Mrinalani <pandeymrinalani02@gmail.com>
2025-01-08 15:56:21 +0530

    add UserHeader component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Button, Container } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 import PostPage from "./assets/pages/PostPage"
 import UserPage from "./assets/pages/UserPage"
+import Header from "./components/Header"
 
 
 function App() {
 
   return (
      <Container maxW='620px'>
+      <Header />
       <Routes>
         <Route path="/:username" element={<UserPage />}/>
         <Route path="/:username/post:pId" element={<PostPage />}/>
