c1d55292aecd413496f2f8cad674e2dc9c010cce
Mrinalani <pandeymrinalani02@gmail.com>
2025-01-12 19:36:44 +0530

    add like and replies

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,78 +1,172 @@
 import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 
-export const createPost = async(req,res) => {
-    try {
-        const {postedBy, text, img} = req.body;
+export const createPost = async (req, res) => {
+  try {
+    const { postedBy, text, img } = req.body;
 
-        if(!text || !postedBy) {
-        return res.status(400).json({ message: "postedBy and text field is required" });
-        }
-
-        const user = await User.findById(postedBy);
-
-        if(!user){
-        return res.status(404).json({ message: "User not found" });
-        }
-        if(user._id.toString() !== req.user._id.toString()){
-        return res.status(401).json({ message: "Unauthorized to create post" });  
-        }
-
-        if(text.length > 500){
-        return res.status(400).json({ message: "Text must be less than 500 characters"});
-        }
-
-        const newPost = new Post({
-            postedBy,
-            text,
-            img
-        })
-
-        await newPost.save()
-        return res.status(201).json({ message: "post created successfully", newPost});
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-        console.log("error in create post", error.message);
+    if (!text || !postedBy) {
+      return res
+        .status(400)
+        .json({ message: "postedBy and text field is required" });
     }
-}
 
-export const getPost = async(req,res) => {
-try {
-    const {id} = req.params;
+    const user = await User.findById(postedBy);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user._id.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to create post" });
+    }
+
+    if (text.length > 500) {
+      return res
+        .status(400)
+        .json({ message: "Text must be less than 500 characters" });
+    }
+
+    const newPost = new Post({
+      postedBy,
+      text,
+      img,
+    });
+
+    await newPost.save();
+    return res
+      .status(201)
+      .json({ message: "post created successfully", newPost });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("error in create post", error.message);
+  }
+};
+
+export const getPost = async (req, res) => {
+  try {
+    const { id } = req.params;
     const post = await Post.findById(id);
 
-    if(!post){
-    res.status(404).json({ message: "Post not Found" });
+    if (!post) {
+      res.status(404).json({ message: "Post not Found" });
     }
 
     res.status(200).json({ message: "Post found", post });
-
-} catch (error) {
+  } catch (error) {
     res.status(500).json({ message: error.message });
     console.log("error in get post", error.message);
-}
-}
+  }
+};
 
-export const deletePost = async(req,res) => {
-    try {
-        const {id} = req.params;
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        const post = await Post.findById(id);
+    const post = await Post.findById(id);
 
-        if(!post){
-        return res.status(404).json({ message: "Post not found" });
-        }
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
 
-        if(post.postedBy.toString() !== req.user._id.toString()){
-        return res.status(401).json({ message: "Unauthorized to delete post" });  
-        }
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "Unauthorized to delete post" });
+    }
 
-        await Post.findByIdAndDelete(id)
+    await Post.findByIdAndDelete(id);
 
-        return res.status(200).json({ message: "Post deleted successfully" });  
-
-    } catch (error) {
+    return res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
     res.status(500).json({ message: error.message });
     console.log("error in delete post", error.message);
+  }
+};
+
+export const likeUnLikePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await Post.findById(id);
+    const user_id = req.user._id;
+
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+    }
+
+    console.log(post);
+    const isLiked = post.likes.includes(user_id);
+
+    if (isLiked) {
+      await Post.updateOne({ _id: id }, { $pull: { likes: user_id } });
+      res.status(200).json({ message: "Post UnLiked successfully" });
+    } else {
+      await Post.updateOne({ _id: id }, { $push: { likes: user_id } });
+      res.status(200).json({ message: "Post Liked successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("error in like post", error.message);
+  }
+};
+
+export const replyToPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+    const userProfilePic = req.user.profilePic;
+    const username = req.user.username;
+
+    if (!text) {
+      res.status(400).json({ message: "Text field is required" });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post Not Found" });
+    }
+
+    const reply = { userId, text, userProfilePic, username };
+
+    post.replies.push(reply);
+    await post.save();
+
+    return res.status(200).json({ message: "Reply added successfully", post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("error in reply to post", error.message);
+  }
+};
+
+export const getFeedPost = async(req,res) => {
+    try {
+        console.log("hello")
+
+        const userId = req.user._id;
+        console.log(userId)
+
+        const user = await User.findById(userId)
+        console.log(user)
+
+
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found" });
+          }
+
+          const following = user.following;
+          console.log(following)
+
+
+          const feedPost = await Post.find({postedBy: {$in: following}}).sort({createdAt: -1});
+          console.log(feedPost)
+
+
+          return res.status(200).json((feedPost))
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    console.log("error in feed post", error.message);
     }
 }
+
+
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { createPost, deletePost, getPost } from '../controllers/postController.js';
+import { createPost, deletePost, getFeedPost, getPost, likeUnLikePost, replyToPost } from '../controllers/postController.js';
 import { protectRoute } from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
-router.get("/:id", getPost)
-router.post("/create",protectRoute, createPost)
-router.delete("/:id",protectRoute, deletePost)
+router.get("/feed", protectRoute, getFeedPost);         
+router.post("/create", protectRoute, createPost);       
+router.post("/like/:id", protectRoute, likeUnLikePost); 
+router.post("/reply/:id", protectRoute, replyToPost);   
+router.delete("/:id", protectRoute, deletePost);        
+router.get("/:id", getPost);                           
+
+
+
+
 
 export default router;
